f952334bbfb3de8a407d82ccecff7f403a04f901
Key Yu Wan <keyflcbyuwan@gmail.com>
2025-12-14 15:09:10 -0300

    feat: incomes screen

--- src/routes/incomes.tsx
+++ src/routes/incomes.tsx
@@ -1,9 +1,58 @@
+import { BudgetCategoryCard } from "@/components/app/budget-category-card";
+import { FinancialSummaryCard } from "@/components/app/financial-summary-card";
 import { createFileRoute } from "@tanstack/react-router";
+import {
+  BanknoteArrowUpIcon,
+  CalendarArrowUpIcon,
+  CodeIcon,
+  DollarSignIcon,
+} from "lucide-react";
 
 export const Route = createFileRoute("/incomes")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/incomes"!</div>;
+  return (
+    <div className="relative flex flex-1 flex-col gap-4">
+      <h1 className="text-2xl font-bold">Entradas</h1>
+
+      <div className="flex flex-col gap-2">
+        <FinancialSummaryCard
+          title="Entradas"
+          amount={15_500.34}
+          icon={BanknoteArrowUpIcon}
+          iconClassName="text-primary"
+        />
+
+        <FinancialSummaryCard
+          title="A receber"
+          amount={5_500.34}
+          icon={CalendarArrowUpIcon}
+          iconClassName="text-warning"
+        />
+
+        <FinancialSummaryCard
+          title="Total previsto"
+          amount={5_500.34}
+          icon={DollarSignIcon}
+        />
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <h2 className="text-xl font-medium">Receitas</h2>
+
+        <div className="scrollbar-none -mx-5 flex gap-2.5 overflow-x-auto px-5 py-2">
+          <BudgetCategoryCard
+            title="Software Dev."
+            pendingAmount={6_000}
+            totalAmount={12_000}
+            href="/"
+            icon={CodeIcon}
+            iconWrapperClassName="bg-zinc-400/10"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
